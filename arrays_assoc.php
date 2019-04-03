35bb8169c3779435198ba1f5735797f6b9e48aa7
anikonchuk <anikonch@gmail.com>
2019-04-03 16:32:36 -0400

    Add arrays_assoc.php about associative arrays

---
+++ arrays_assoc.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+<?php
+
+$numberList = array(10, 20, 49);
+
+$names = array("first_name" => 'Edwin', "last_name" => 'Diaz');
+echo $names['first_name'];
+//Edwin
+ ?>
+  </body>
+</html>
