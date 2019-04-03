ea0ac16269d961b4a485a2b2f815c69da85c5708
anikonchuk <anikonch@gmail.com>
2019-04-03 15:57:47 -0400

    Add math.php

---
+++ math.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+<?php
+
+echo 56 + 45;
+//automatically calculates
+echo "<br />";
+echo 56 - 45;
+echo "<br />";
+echo 56 * 45;
+echo "<br />";
+echo 56 / 45;
+echo "<br />";
+echo 4 + 3 * 2 - 1;
+echo "<br />";
+echo (5 + 5) * 10;
+echo "<br />";
+echo 56 * 45;
+
+$number = 12;
+$number2 = 24;
+
+echo $number1 + $number2;
+
+ ?>
+  </body>
+</html>
