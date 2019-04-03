36f9f75504a65465684bd5fe52583b4df37bf762
anikonchuk <anikonch@gmail.com>
2019-04-03 16:20:13 -0400

    Add arrays.php

---
+++ arrays.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+<?php
+
+$numberList = array(3, "hello", 258);
+$numberList2 = [3, 5, 2, 8];
+
+//can't just echo $numberList
+echo $numberList[0];
+echo "<br  />";
+print_r($numberList);
+
+ ?>
+  </body>
+</html>
