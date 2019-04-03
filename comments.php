5f6c21ff75ce8f72768105ecf39b97c08478f666
anikonchuk <anikonch@gmail.com>
2019-04-03 11:05:33 -0400

    Add comments.php

---
+++ comments.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+      // This function below displays text
+
+      // this is the second line
+      echo "Hello Student";
+      /* This is a multiline
+      comment */
+
+     ?>
+
+  </body>
+</html>
