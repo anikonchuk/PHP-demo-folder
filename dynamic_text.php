5a882604c0fb6d5810ebfb39efd11ff35c7a63db
anikonchuk <anikonch@gmail.com>
2019-04-03 10:55:23 -0400

    First Commit

---
+++ dynamic_text.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Hello Student</h1>
+
+  </body>
+</html>
