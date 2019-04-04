58aca59111068a68b30b40badcf84d096718ccb1
anikonchuk <anikonch@gmail.com>
2019-04-04 15:04:47 -0400

    Add while_loop.php

---
+++ while_loop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+    $counter = 0
+    while($counter < 10){
+      echo "Hello Student";
+      $counter += 1;
+    }
+
+
+
+
+     ?>
+  </body>
+</html>
