d92a9f01b372f9c471cc78bc5bd00d17c3f0dafd
anikonchuk <anikonch@gmail.com>
2019-04-04 14:06:05 -0400

    Add ifstatements.php

---
+++ ifstatement.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+      if(3 > 10){
+
+        echo "three is less than 10";
+
+      } elseif(4 < 5) {
+        echo "four is less than 5";
+      } else {
+        echo "None of my conditions were true";
+      }
+
+     ?>
+
+  </body>
+</html>
