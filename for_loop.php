6c1410ff89b9652f4bd7561e2cc9ca7d645ecb32
anikonchuk <anikonch@gmail.com>
2019-04-04 15:12:23 -0400

    Add for_each.php

---
+++ for_loop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    for($counter = 0; $counter < 10; $counter++){
+      echo $counter . "<br />";
+    }
+
+
+     ?>
+
+  </body>
+</html>
