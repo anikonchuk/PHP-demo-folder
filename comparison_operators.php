45bf79033a61d366b2499531f40585695b1f8d35
anikonchuk <anikonch@gmail.com>
2019-04-04 14:20:19 -0400

    Add comparison and logical operations

---
+++ comparison_operators.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+    /*
+    equal ==
+    identical ===
+    compare < > >= <= <>
+    not equal !=
+    not identical !==
+
+    Logical operators
+    And &&
+    Or ||
+    Not !
+    */
+    if (4 < 4) {
+      echo "it is true";
+    }
+
+     ?>
+
+  </body>
+</html>
