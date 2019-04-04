0d9cbbb1ef370044f6d9eebb4890565edfdd20c0
anikonchuk <anikonch@gmail.com>
2019-04-04 14:54:39 -0400

    Add switchstatements.php

---
+++ switchstatements.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+    $number = 100;
+
+    switch($number) {
+      case 34:
+      echo "it is 34";
+      break;
+      case 100:
+      echo "it is 100";
+      break;
+      default:
+      echo "we could not find anything";
+      break;
+    }
+
+
+
+     ?>
+
+
+  </body>
+</html>
