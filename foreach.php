c8bb566acfa08752d2744a5178223129711fbb30
anikonchuk <anikonch@gmail.com>
2019-04-04 15:24:27 -0400

    Add foreach.php

---
+++ foreach.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+    $numbers = array(345, 234, 90, 243);
+    foreach($numbers as $number) {
+      echo $number . "<br />";
+    }
+
+     ?>
+  </body>
+</html>
