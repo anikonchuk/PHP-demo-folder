f9326a3dfaa7b323e5822aab7d8f632ac3f23da9
anikonchuk <anikonch@gmail.com>
2019-04-06 16:43:22 -0400

    Add constants.php

---
+++ constants.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+
+    define("NAME", 1000);
+
+    echo NAME;
+    //1000
+
+
+     ?>
+
+  </body>
+</html>
