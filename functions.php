f00bd871069994255df3a69470ba4249fa5537c4
anikonchuk <anikonch@gmail.com>
2019-04-06 16:07:09 -0400

    Add functions.php

---
+++ functions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+<?php
+
+  function say_something(){
+    echo "Hello Student, do you like the class?";
+
+  }
+
+  function init() {
+    say_something();
+  }
+
+  init();
+
+ ?>
+
+
+  </body>
+</html>
