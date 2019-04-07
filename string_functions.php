4b21958a3c014db6e5dc5f2ea15925fc0bee8d84
anikonchuk <anikonch@gmail.com>
2019-04-07 11:45:10 -0400

    Add string_functions.php

---
+++ string_functions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+    $string = "Hello Student do you like the class?";
+    echo strlen($string);
+    echo "<br />";
+
+    echo strtoupper($string);
+    echo "<br />";
+
+    echo strtolower($string);
+    echo "<br />";
+
+    print $string;
+
+
+     ?>
+
+  </body>
+</html>
