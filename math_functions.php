d6807efe0ea99240ead136d3799909cfa0ca870b
anikonchuk <anikonch@gmail.com>
2019-04-07 10:53:03 -0400

    Add math_functions.php

---
+++ math_functions.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+
+    echo pow(2,7);
+    //128 2 to the 7th power
+    echo "<br />";
+    echo rand();
+    // gives a random Number
+    echo "<br />";
+    echo rand(1, 1000);
+    //gives random number between 1 and 1000
+    echo "<br />";
+
+    echo sqrt(100);
+    echo "<br />";
+
+    echo ceil(4.6);
+    //rounds up to next integer
+    echo "<br />";
+    echo floor(4.6);
+    //rounds down to integer
+
+    echo "<br />";
+    echo round(4.5);
+    //rounds to closest integer up or down
+    
+
+
+     ?>
+
+  </body>
+</html>
