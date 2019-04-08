b97da70eb61336e688872cbb43d3a43a3da64205
anikonchuk <anikonch@gmail.com>
2019-04-08 11:04:14 -0400

    Create mysql folder and login.php. Add form markup to login.php

---
+++ mysql/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MySQL Demo</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="col-xs-6">
+          <form class="" action="login.php" method="post">
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" class="form-control" id="username" name="username">
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" id="password" name="password">
+            </div>
+              <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+          </form>
+
+      </div>
+    </div>
+
+  </body>
+</html>
