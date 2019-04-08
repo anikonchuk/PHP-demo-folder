318e7ddaa1301e1d7e437c08b43a42f671b3f703
anikonchuk <anikonch@gmail.com>
2019-04-08 16:52:17 -0400

    Move SQL query from login_update to functions

--- mysql/functions.php
+++ mysql/functions.php
@@ -17,3 +17,21 @@ function showAllData() {
   }
 
 }
+
+function updateTable(){
+  global $connection;
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $id = $_POST['id'];
+
+  $query = "UPDATE users SET ";
+  $query .= "username  = '$username', ";
+  $query .= "password = '$password' ";
+  $query .= "WHERE id = $id";
+
+  $result = mysqli_query($connection, $query);
+
+  if(!$result) {
+    die("QUERY FAILED " . mysqli_error($connection));
+  }
+}
