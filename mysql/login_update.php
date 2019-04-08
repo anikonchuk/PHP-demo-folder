3d7ccf8aac02f711f2e467da71d3fa3cfe4c4089
anikonchuk <anikonch@gmail.com>
2019-04-08 16:46:49 -0400

    Update mysql/login_update.php to include SQL query to update data in DB

--- mysql/login_update.php
+++ mysql/login_update.php
@@ -1,6 +1,29 @@
 <?php include "db.php"; ?>
 <?php include "functions.php"; ?>
 
+<?php
+  if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $id = $_POST['id'];
+
+    $query = "UPDATE users SET ";
+    $query .= "username  = '$username', ";
+    $query .= "password = '$password' ";
+    $query .= "WHERE id = $id";
+
+    $result = mysqli_query($connection, $query);
+
+    if(!$result) {
+      die("QUERY FAILED " . mysqli_error($connection));
+    }
+
+  }
+
+
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -12,7 +35,7 @@
 
     <div class="container">
       <div class="col-sm-6">
-        <form class="" action="login_create.php" method="post">
+        <form class="" action="login_update.php" method="post">
           <div class="form-group">
             <label for="username">Username</label>
             <input type="text" class="form-control" id="username" name="username">
