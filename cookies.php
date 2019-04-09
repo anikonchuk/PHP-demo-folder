134a6550f60d5efbb65d836b89a125593f417784
anikonchuk <anikonch@gmail.com>
2019-04-09 15:57:31 -0400

    Add cookies.php and setcookie() for 1 week expiration

---
+++ cookies.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = "SomeName";
+$value = 100;
+$expiration = time() + (60*60*24*7);
+
+setcookie($name, $value, $expiration);
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+
+
+
+     ?>
+
+  </body>
+</html>
