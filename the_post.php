66a81158900ea19eb92aa9bfd822a4d820da7f51
anikonchuk <anikonch@gmail.com>
2019-04-09 15:36:41 -0400

    Add the_post.php to explore  superglobal

---
+++ the_post.php
@@ -0,0 +1,20 @@
+<?php
+
+echo $_POST['name'];
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <form class="" action="the_post.php" method="post">
+      <input type="text" name="name" value="">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+
+  </body>
+</html>
