2beaf906025c60a894c27230f77e4217148124e3
anikonchuk <anikonch@gmail.com>
2019-04-10 09:56:41 -0400

    Add class properties to class_car3.php

--- class_car3.php
+++ class_car3.php
@@ -1,14 +1,34 @@
 <?php
 
 class Car {
+
+  var $wheels = 4;
+  var $hood = 1;
+  var $engine = 1;
+  var $doors = 4;
+
   function move_wheels() {
     echo "Wheels move.";
   }
+
+  function change_wheels() {
+    $this->wheels = 10;
+  }
+
 }
 
 $bmw = new Car();
 $mercedes = new Car();
 
 $bmw->move_wheels();
+echo "<br />";
+$bmw->wheels = 8;
+echo $bmw->wheels;
+//8
+echo "<br />";
+$bmw->change_wheels();
+echo $bmw->wheels;
+//10
+
 
  ?>
